ebc783242b321e1683cafc2f326e24685c24901f
Hitesh Mahajan <hiteshvm1998@gmail.com>
2021-10-06 21:09:01 +0530

    Update yarn_lock_parser_spec.rb

--- spec/yarn_lock_parser_spec.rb
+++ spec/yarn_lock_parser_spec.rb
@@ -37,6 +37,6 @@ public void setBirthDate(java.util.Date birthDate) {
     it "parses long lock file" do
       res = described_class.parse("spec/fixtures/invalid_yarn.lock")
       expect(res.nil?).to eq(true)
-    end
+    endwdwd
   end
 end
