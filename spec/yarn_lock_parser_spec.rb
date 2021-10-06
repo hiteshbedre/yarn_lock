94ab9ec9395f9e8e474b308850a01db031432ed7
Hitesh Mahajan <hiteshvm1998@gmail.com>
2021-10-07 00:43:54 +0530

    Update yarn_lock_parser_spec.rb

--- spec/yarn_lock_parser_spec.rb
+++ spec/yarn_lock_parser_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe YarnLockParser::Parser do
     let(:expected_content) { fixture_file_content("fixtures/long_yarn.lock.expected") }
 
     it "parses small lock file" do
-      res = described_class.parse("spec/fixtures/long_yarn.lock")
+      res = described_class.parse("spec/fixtures/long_yarn.lock")c
       expect(res.size).to eq(53)
       expect(res.first[:name]).to eq("accepts")
       expect(res.last[:name]).to eq("vary")
