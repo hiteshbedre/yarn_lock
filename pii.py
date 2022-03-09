bfe84774a6e22b2d5898a790e477c93037ed6d45
hiteshbedre <32206192+hiteshbedre@users.noreply.github.com>
2022-03-09 19:18:21 +0530

    Create pii.py

---
+++ pii.py
@@ -0,0 +1,12 @@
+password <code>demo</code>.
+    </p>
+    <label className={form__label}>
+      Username
+      <input
+        className={form__input}
+        type="text"
+<p>
+      For this demo, please log in with the username <code>gatsby</code> and the
+      password <code>demo</code>.
+    </p>
+    <label className={form__label}>
